c1b0b8882165c1bb3d94f45b13bfdc37f46ca60c
成田陽紀 <naritaharuki@naritaharuki.local>
2025-03-29 22:50:35 +0900

    返信されるように

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,64 @@
 import express from 'express'
-import { DB } from './db'
+import axios from 'axios'
+import dotenv from 'dotenv'
+import fs from 'fs'
 
 const app = express()
 const port = 3000
 
+dotenv.config()
 app.use(express.json())
 
-DB.initialize()
-  .then(() => console.log('MySQL接続成功'))
-  .catch((err) => console.error('データベース接続エラー:', err))
+app.post('/webhook', async (req, res) => {
+  const events = req.body.events
+  if (!events || events.length === 0) {
+    writeLog(`req ${req.body}`)
+    res.status(400).json({ error: 'No events found' });
+    return;
+  }
 
-app.post('/webhook', (req, res) => {
-  const message = req.body.message
-  res.json({ reply: message })
+  const event = events[0]
+  const message = event.message
+  const replyToken = event.replyToken
+
+  await sendReplyMessage(replyToken, `${message.text}だと？黙れ！`, event.source.userId)
+  res.status(200).send()
 })
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`)
 })
+
+const sendReplyMessage = async (replyToken:string, messageText:string,userId:string) => {
+  const replyMessage = {
+    replyToken: replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: messageText,
+      },
+    ],
+  };
+
+  try {
+    await axios.post('https://api.line.me/v2/bot/message/reply', replyMessage, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': process.env.CHANNEL_ACCESS_TOKEN,
+      },
+    })
+    writeLog(`res ${userId} ${messageText}だと？黙れ！`)
+  } catch (error) {
+    writeLog(`res ${userId} Error sending reply message: ${error}`)
+  }
+}
+
+const writeLog = (logMessage: string) => {
+  const logFilePath = '/ore-no.log'
+  const timestamp = new Date().toISOString()
+  const logEntry = `[${timestamp}] ${logMessage}\n`
+  
+  fs.appendFile(logFilePath, logEntry, (err) => {
+    if (err) console.error('Error writing log:', err)
+  })
+}
