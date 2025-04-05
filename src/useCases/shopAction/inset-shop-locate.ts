4632d021d7e235ff7106a504b775683bb1532304
成田陽紀 <naritaharuki@naritaharuki.local>
2025-04-05 19:59:27 +0900

    db initialized

--- src/useCases/shopAction/inset-shop-locate.ts
+++ src/useCases/shopAction/inset-shop-locate.ts
@@ -3,7 +3,7 @@ import SHOP_STATUS from "../../type/shop-status"
 
 const insertShopLocate = async (message: string, userId: string) => {
     DBORM.Shop.update(userId, { locate: message })
-    DBORM.User.updateStatus(userId, SHOP_STATUS.third.wannaInsertShopURL)
+    DBORM.User.updateStatus(userId, SHOP_STATUS.third.insertShopURL)
 
     const messageText = `お店の場所を「${message}」で登録しました。
 最後に、お店のURLを登録します。あなたのお店のURLを教えてください。`
